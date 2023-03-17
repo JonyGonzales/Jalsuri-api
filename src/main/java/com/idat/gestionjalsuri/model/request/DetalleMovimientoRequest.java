08368c128a955551d175ff29ff4608593f608e82
Jony Gonzales Gonzales <Jony.gonzales.g@gmail.com>
2023-03-16 19:36:25 -0500

    Update DetalleMovimientoRequest.java

--- src/main/java/com/idat/gestionjalsuri/model/request/DetalleMovimientoRequest.java
+++ src/main/java/com/idat/gestionjalsuri/model/request/DetalleMovimientoRequest.java
@@ -1,5 +1,8 @@
 package com.idat.gestionjalsuri.model.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +11,13 @@ import lombok.Setter;
 public class DetalleMovimientoRequest {
 
     private String observaciones;
+    @NotNull
+	@NotBlank
     private Long idMovAlmacen;
+    @NotNull
+	@NotBlank
     private Long idProducto;
+    @NotNull
+	@NotBlank
     private Integer cantidad;
 }
